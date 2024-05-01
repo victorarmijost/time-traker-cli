231b073e56c9e323b9eeccbc1e12a0a31300867d
Victor Armijo <varmijo@stocktwits.com>
2024-04-30 18:01:01 -0600

    remove_pomodoro

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -19,33 +19,15 @@ func formatDuration(d float64) string {
 
 func getPrompt(state *state.State) repl.Prompt {
 	var (
-		wt, ct, pt, tt           float64
-		pomCount, pomProg        int
-		pomStatus, lastPomStatus string
-		firstCall                bool = true
+		wt, ct, pt, tt float64
 	)
 
 	return func(pk repl.PromptType) string {
-		defer func() {
-			firstCall = false
-		}()
-
 		if pk == repl.FULL_UPDATE {
 			wt = localStore.GetTimeByStatus(state.Date, localStore.StatusPending)
 			ct = localStore.GetTimeByStatus(state.Date, localStore.StatusCommited)
 			pt = localStore.GetTimeByStatus(nil, localStore.StatusPool)
 			tt = state.GetTaskTime(nil)
-
-			pomCount, pomProg, pomStatus = pomodoro(float64(wt + tt))
-
-			if firstCall {
-				lastPomStatus = pomStatus
-			}
-
-			if pomStatus != lastPomStatus && pomCount > 0 {
-				pomodoroAlert(pomStatus)
-				lastPomStatus = pomStatus
-			}
 		}
 
 		statusBar := ""
@@ -69,8 +51,6 @@ func getPrompt(state *state.State) repl.Prompt {
 		if state.Date != nil {
 			statusBar = fmt.Sprintf("%s[%s]", statusBar, state.Date.Format("06-01-02"))
 		}
-
-		statusBar = fmt.Sprintf("%s[%dP][%d%%][%s]", statusBar, pomCount, pomProg, pomStatus)
 
 		if statusBar != "" {
 			return fmt.Sprintf("%s tt", statusBar)
